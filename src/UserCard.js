f13c2a0157f8c1c6c0995d7632f290053ee078c1
Joshua MacSween <joshmacsween@gmail.com>
2020-08-03 17:21:59 -0600

    basic component

--- src/UserCard.js
+++ src/UserCard.js
@@ -1,8 +1,10 @@
 import React from 'react'
+// import gravatar from './gravatar.jpg'
 
 function UserCard() {
   return (
-    <div>
+    <div class="UserCard">
+      {/* <img gravatar alt=""/> */}
       <h3>Name: </h3>
       <p>Description: </p>
     </div>
